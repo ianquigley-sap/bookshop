16b95c032e6531431b2605ce0ba041223f8916a5
Ian Quigley <ian.quigley@sap.com>
2019-05-09 16:30:13 +0100

    odata version

--- functions/Author.js
+++ functions/Author.js
@@ -1,6 +1,7 @@
 exports.handler = async (event, context) => {
     return {
       statusCode: 200,
-      body: '{"ARSE@odata.context":"$metadata#Authors","@odata.metadataEtag":"W/\"RcF07THJp5aOwufXCQWQTVDG5Ow8wVuVuKPYLd9dK2Y=\"","value":[{"ID":101,"firstName":"Emily","lastName":"Brontë"},{"ID":107,"firstName":"Charlotte","lastName":"Brontë"},{"ID":150,"firstName":"Edgar Allen","lastName":"Poe"},{"ID":170,"firstName":"Richard","lastName":"Carpenter"}]}'
+      headers: { "OData-Version": "2.0"},
+      body: '{"@odata.context":"$metadata#Authors","@odata.metadataEtag":"W/\"RcF07THJp5aOwufXCQWQTVDG5Ow8wVuVuKPYLd9dK2Y=\"","value":[{"ID":101,"firstName":"Emily","lastName":"Brontë"},{"ID":107,"firstName":"Charlotte","lastName":"Brontë"},{"ID":150,"firstName":"Edgar Allen","lastName":"Poe"},{"ID":170,"firstName":"Richard","lastName":"Carpenter"}]}'
     };
   };
