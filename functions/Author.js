2d9faafd97353a5448d392d061e85b1c5ec2768d
Ian Quigley <ian.quigley@sap.com>
2019-05-09 16:41:57 +0100

    Update Author.js

--- functions/Author.js
+++ functions/Author.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
     return {
       statusCode: 200,
-      headers: { "OData-Version": "4.0"},
+      headers: { "OData-Version": "4.0", "Content-Type": "application/json; odata.metadata=minimal"},
       body: '{"@odata.context":"$metadata#Authors","@odata.metadataEtag":"W/\"RcF07THJp5aOwufXCQWQTVDG5Ow8wVuVuKPYLd9dK2Y=\"","value":[{"ID":101,"firstName":"Emily","lastName":"Brontë"},{"ID":107,"firstName":"Charlotte","lastName":"Brontë"},{"ID":150,"firstName":"Edgar Allen","lastName":"Poe"},{"ID":170,"firstName":"Richard","lastName":"Carpenter"}]}'
     };
   };
