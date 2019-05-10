ad9addcbc7916d8d4125422fa9fb75cba51dd114
Ian Quigley <ian.quigley@sap.com>
2019-05-10 15:26:09 +0100

    Adding metadata function

---
+++ functions-source/Metadata.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/xml"
+    },
+    body: getMetadata()
+  };
+};
+
+function getMetadata() {
+  var metadataPath = path.join(
+    __dirname,
+    "ui",
+    "webapp",
+    "localService",
+    "metadata.xml"
+  );
+  const metadata = fs.readFileSync("." + metadataPath, "utf8");
+  return metadata;
+}
