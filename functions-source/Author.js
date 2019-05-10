2a58bb8512bc0a4ee8f18522cad148565b7cd192
Ian Quigley <ian.quigley@sap.com>
2019-05-10 11:51:51 +0100

    f

--- functions-source/Author.js
+++ functions-source/Author.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
     const ran = randomstring.generate();
     return {
       statusCode: 200,
-      headers: { "OData-Version": "4.0", "Content-Type": "application/json; odata.metadata=minimal"},
+      headers: { "OData-Version": "4.0", "Content-Type": "application/json;odata.metadata=minimal"},
       body: '{"@odata.context":"$metadata#Authors","@odata.metadataEtag":"W/\"RX6mBWmwH9Zeh0gcGabUFi0apSdqLi5g86PyKQQ8rzo=\"","value":[{"ID":"1","firstName":"Marcel","lastName":"Proust"},{"ID":"2","firstName":"Miguel","lastName":"de Cervantes"},{"ID":"3","firstName":"James","lastName":"Joyce"},{"ID":"4","firstName":"F.","lastName":"Scott Fitzgerald"}]}'
     };
   };
