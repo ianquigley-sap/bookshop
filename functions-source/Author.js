a5b7bd334f7244104b0a375a7fd18131ba64d73f
Ian Quigley <ian.quigley@sap.com>
2019-05-10 11:54:06 +0100

    f

--- functions-source/Author.js
+++ functions-source/Author.js
@@ -6,6 +6,6 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       headers: { "OData-Version": "4.0", "Content-Type": "application/json;odata.metadata=minimal"},
-      body: '{"@odata.context":"$metadata#Authors","@odata.metadataEtag":"W/\"RX6mBWmwH9Zeh0gcGabUFi0apSdqLi5g86PyKQQ8rzo=\"","value":[{"ID":"1","firstName":"Marcel","lastName":"Proust"},{"ID":"2","firstName":"Miguel","lastName":"de Cervantes"},{"ID":"3","firstName":"James","lastName":"Joyce"},{"ID":"4","firstName":"F.","lastName":"Scott Fitzgerald"}]}'
+      body: '{"@odata.context":"$metadata#Authors","@odata.metadataEtag":"WRX6mBWmwH9Zeh0gcGabUFi0apSdqLi5g86PyKQQ8rzo=\"","value":[{"ID":"1","firstName":"Marcel","lastName":"Proust"},{"ID":"2","firstName":"Miguel","lastName":"de Cervantes"},{"ID":"3","firstName":"James","lastName":"Joyce"},{"ID":"4","firstName":"F.","lastName":"Scott Fitzgerald"}]}'
     };
   };
