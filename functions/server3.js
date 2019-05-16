6dbee847051094f19d6abd891f816d9a178c48db
Austin Devine <austin.devine@sap.com>
2019-05-16 19:55:46 +0100

    fixes

--- functions/server3.js
+++ functions/server3.js
@@ -1,5 +1,7 @@
-'use strict';
 const cds = require('@sap/cds')
+
+var morgan = require('morgan')
+
 
 if (1===2){
 require('../srv/cat-service.cds')
@@ -7,15 +9,19 @@ require('../srv/cat-service')
 require('../db/data-model.cds')
 }
 
+// testing
 
 
+'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 
+app.use(morgan('dev'))
 
 cds.serve('CatalogService').from('srv').in(app)
-cds.serve('all').in(app).at('/.netlify/functions/catalog/')
+cds.serve('all').in(app).at('/.netlify/functions/server3') // path must route to lambda
+// app.listen()
 
 module.exports = app;
 module.exports.handler = serverless(app);
